b0302bf45a75b988deb1ccef33b5d3e4fe7ddea1
empzxb <54690707@qq.com>
2021-03-17 15:40:24 +0800

    reel

--- facebook-clone/src/StoryReel.js
+++ facebook-clone/src/StoryReel.js
@@ -16,24 +16,26 @@ function StoryReel({ src, selected, Icon, title }) {
     	/>
     	<Story
     		image="https://scontent-hkg4-2.xx.fbcdn.net/v/t1.0-9/152709560_10160371652501756_3739079147937072459_o.jpg?_nc_cat=110&ccb=3&_nc_sid=5b7eaf&_nc_ohc=Y6k6Of7kjAYAX9NXKVx&_nc_ht=scontent-hkg4-2.xx&oh=4d07058ac3329ecaa46e45ef5d0cfb33&oe=605FD662"
-    		profileSrc={ user.photoURL }  
-    		title={ user.displayName }
+    		profileSrc="https://avatars2.githubusercontent.com/u/22930837"
+    		title="xibo"
     	/>
-    	<Story
-    		image="https://scontent-hkg4-2.xx.fbcdn.net/v/t1.0-9/152709560_10160371652501756_3739079147937072459_o.jpg?_nc_cat=110&ccb=3&_nc_sid=5b7eaf&_nc_ohc=Y6k6Of7kjAYAX9NXKVx&_nc_ht=scontent-hkg4-2.xx&oh=4d07058ac3329ecaa46e45ef5d0cfb33&oe=605FD662"
-    		profileSrc={ user.photoURL }  
-    		title={ user.displayName }
-    	/>
-    	<Story
-    		image="https://scontent-hkg4-2.xx.fbcdn.net/v/t1.0-9/152709560_10160371652501756_3739079147937072459_o.jpg?_nc_cat=110&ccb=3&_nc_sid=5b7eaf&_nc_ohc=Y6k6Of7kjAYAX9NXKVx&_nc_ht=scontent-hkg4-2.xx&oh=4d07058ac3329ecaa46e45ef5d0cfb33&oe=605FD662"
-    		profileSrc={ user.photoURL } 
-    		title={ user.displayName }
-    	/>
-    	<Story
-    		image="https://scontent-hkg4-2.xx.fbcdn.net/v/t1.0-9/152709560_10160371652501756_3739079147937072459_o.jpg?_nc_cat=110&ccb=3&_nc_sid=5b7eaf&_nc_ohc=Y6k6Of7kjAYAX9NXKVx&_nc_ht=scontent-hkg4-2.xx&oh=4d07058ac3329ecaa46e45ef5d0cfb33&oe=605FD662"
-    		profileSrc={ user.photoURL } 
-    		title={ user.displayName }
-    	/>
+        <Story
+            image="https://scontent-hkg4-2.xx.fbcdn.net/v/t1.0-9/152709560_10160371652501756_3739079147937072459_o.jpg?_nc_cat=110&ccb=3&_nc_sid=5b7eaf&_nc_ohc=Y6k6Of7kjAYAX9NXKVx&_nc_ht=scontent-hkg4-2.xx&oh=4d07058ac3329ecaa46e45ef5d0cfb33&oe=605FD662"
+            profileSrc="https://avatars2.githubusercontent.com/u/22930837"
+            title="xibo"
+        />
+        <Story
+            image="https://scontent-hkg4-2.xx.fbcdn.net/v/t1.0-9/152709560_10160371652501756_3739079147937072459_o.jpg?_nc_cat=110&ccb=3&_nc_sid=5b7eaf&_nc_ohc=Y6k6Of7kjAYAX9NXKVx&_nc_ht=scontent-hkg4-2.xx&oh=4d07058ac3329ecaa46e45ef5d0cfb33&oe=605FD662"
+            profileSrc="https://avatars2.githubusercontent.com/u/22930837"
+            title="xibo"
+        />
+
+        <Story
+            image="https://scontent-hkg4-2.xx.fbcdn.net/v/t1.0-9/152709560_10160371652501756_3739079147937072459_o.jpg?_nc_cat=110&ccb=3&_nc_sid=5b7eaf&_nc_ohc=Y6k6Of7kjAYAX9NXKVx&_nc_ht=scontent-hkg4-2.xx&oh=4d07058ac3329ecaa46e45ef5d0cfb33&oe=605FD662"
+            profileSrc="https://avatars2.githubusercontent.com/u/22930837"
+            title="xibo"
+        />
+
     </div>
   );
 }
