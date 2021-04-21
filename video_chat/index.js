c5489eb9fc64ff2e4a35eed988339465a5f77d6d
empzxb <54690707@qq.com>
2021-04-21 21:23:10 +0800

    video chat peer simple

---
+++ video_chat/index.js
@@ -0,0 +1,46 @@
+const app = require("express")();
+const https = require('https')
+const fs = require('fs');
+var key = fs.readFileSync('./selfsigned.key');
+var cert = fs.readFileSync('./selfsigned.crt');
+var options = {
+  key: key,
+  cert: cert
+};
+
+//const server = require('https').Server(app)
+const server = https.createServer(options, app);
+const cors = require("cors");
+
+const io = require("socket.io")(server, {
+	cors: {
+		origin: "*",
+		methods: [ "GET", "POST" ]
+	}
+});
+
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+	res.send('Running');
+});
+
+io.on("connection", (socket) => {
+	socket.emit("me", socket.id);
+
+	socket.on("disconnect", () => {
+		socket.broadcast.emit("callEnded")
+	});
+
+	socket.on("callUser", ({ userToCall, signalData, from, name }) => {
+		io.to(userToCall).emit("callUser", { signal: signalData, from, name });
+	});
+
+	socket.on("answerCall", (data) => {
+		io.to(data.to).emit("callAccepted", data.signal)
+	});
+});
+
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
